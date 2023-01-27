ff27edce2f63776c7fd3440f9d1cea5db2a434b1
harshedabdulla <harshed2004@gmail.com>
2023-01-27 23:21:06 +0530

    changes

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,12 +1,22 @@
 import React, {useContext } from 'react'
 import { DataContext } from '../../DataContext'
 
+
 const Home = () => {
-    const { web3Handler, account } = useContext(DataContext)
-    return (
-    <>    
-        {account ? <button href="/profile">{account}</button> : <button onClick={web3Handler}>Login</button>}     
-    </>      
+  const { web3Handler, account } = useContext(DataContext)
+  return (
+    <div className="home-container">
+      <h1 className="med-eth-title">med_eth</h1>
+      {account ?
+        <button className="login-button">
+          <a href="/profile" className="account-text">{account}</a>
+        </button>
+        :
+        <button className="login-button" onClick={web3Handler}>
+          Login
+        </button>
+      }
+    </div>
   )
 }
 
